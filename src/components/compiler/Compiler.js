9ef1b61efec1b26561feca92cb833901b27cb9c8
SeenuNahak <83362881+SeenuNahak@users.noreply.github.com>
2023-05-15 22:46:14 +0530

    Update Compiler.js

--- src/components/compiler/Compiler.js
+++ src/components/compiler/Compiler.js
@@ -11,10 +11,10 @@ function Compiler() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          <strong className="purple">Compiler Design </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Project and Experiments during 6th semester Compiler Design Course
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
         <Col md={4} className="project-card">
