c0f386b7c344febd19acb5177616754a42c1e477
SeenuNahak <83362881+SeenuNahak@users.noreply.github.com>
2023-05-15 21:24:15 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import chatify from "../../Assets/Projects/chatify.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-
+import compiler from "../../Assets/Projects/compiler.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -34,6 +34,16 @@ function Projects() {
               title="Web Dev with Node.js"
               description="Hi I made this project during the 7 Days Free Bootcamp, conducted by SHAPEAI . The instructor during the session was Mr. Shaurya Sinha (Data Analyst Intern at Jio). I got to learn a lot during these 7 days and it was an amazing experience learning with SHAPEAI"
               ghLink="https://github.com/SeenuNahak/ShapeAI_Seenu_Nahak_1.git"
+            />
+          </Col>
+      
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={compiler}
+              isBlog={false}
+              title="Bash-C Compiler"
+              description="This is my 6th semester project for Compiler Design Subject. A Bash to C compiler is a program that takes a Bash script as input and converts it into an equivalent C program. The resulting C program can then be compiled and executed on a variety of platforms, making it a more portable and efficient solution for shell scripting."
+              ghLink="https://github.com/SeenuNahak/Bash-C-Compiler"
             />
           </Col>
         </Row>
